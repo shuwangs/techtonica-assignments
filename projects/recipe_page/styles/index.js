5e8acbb559a327c612daff09749bb1361dd05b64
Shu <shuw425@gmail.com>
2025-10-31 20:37:58 -0400

    Change color of the added ingredient feat: Change newly added ingredient to brown color.

--- projects/recipe_page/styles/index.js
+++ projects/recipe_page/styles/index.js
@@ -1,5 +1,8 @@
-
+// Add Conent 
 let newListItem = document.createElement('li');
 
 newListItem.textContent = 'Salted duck egg yolk';
+newListItem.style.color = "#6c584c"
 document.getElementById('filling_list').appendChild(newListItem);
+
+// Add a check mark as an element to a list
