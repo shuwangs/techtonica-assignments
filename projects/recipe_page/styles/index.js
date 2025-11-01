89ba9a36078260ac225cfb373355bbcf990682d6
Shu <shuw425@gmail.com>
2025-10-31 22:30:15 -0400

    Add check marks as element to `dough_list` feat: Grab the dough list -> HTMLCollections feat: Change HTMLCollections to array feat: loop through to replace previous text as checkbox + label

--- projects/recipe_page/styles/index.js
+++ projects/recipe_page/styles/index.js
@@ -5,4 +5,27 @@ newListItem.textContent = 'Salted duck egg yolk';
 newListItem.style.color = "#6c584c"
 document.getElementById('filling_list').appendChild(newListItem);
 
-// Add a check mark as an element to a list
+
+//
+const doughIngredients = document.getElementById("dough_list");
+const doughItems = doughIngredients.getElementsByTagName("li");
+
+Array.from(doughItems).forEach((item, idx) => {
+
+    let checkbox = document.createElement("input");
+    // Assign checkbox attributes
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = `dough-${idx}`;
+
+    // Create label for the checkbox
+    let label = document.createElement("label");
+    label.htmlFor = checkbox.id;
+    label.textContent = item.textContent;
+    item.textContent = " ";
+    item.appendChild(checkbox);
+    item.appendChild(label);
+  }
+)
+
