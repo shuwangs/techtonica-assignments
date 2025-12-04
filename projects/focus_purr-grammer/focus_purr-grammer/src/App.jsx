a5598a466cb5084149130470484ab387be74b59b
shu.wang <60482034+shuwangs@users.noreply.github.com>
2025-12-04 23:04:49 +0000

    setup folder structures

--- projects/focus_purr-grammer/focus_purr-grammer/src/App.jsx
+++ projects/focus_purr-grammer/focus_purr-grammer/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GameBoard from './components/GameBoard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello， codecademy!
+      <div className="app-container">
+      <GameBoard />
+    </div>
     </>
   )
 }
