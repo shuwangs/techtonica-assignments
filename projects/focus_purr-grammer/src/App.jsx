31bcdae572a002d886a9950a550ea72a2dd0c077
Shu <shuw425@gmail.com>
2025-12-05 01:33:38 -0500

    add CatPlayer component

--- projects/focus_purr-grammer/src/App.jsx
+++ projects/focus_purr-grammer/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import GameBoard from './components/GameBoard'
 
@@ -9,8 +6,8 @@ function App() {
   return (
     <>
       <div className="app-container">
-      <GameBoard />
-    </div>
+        <GameBoard />
+      </div>
     </>
   )
 }
--- projects/focus_purr-grammer/src/components/GameBoard.jsx
+++ projects/focus_purr-grammer/src/components/GameBoard.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { useState } from "react";
 import HeaderBar from "./HeaderBar";
-import FallingItems from "./FallingItems";
 import CatPlayer from "./CatPlayer";
+// import FallingItems from "./FallingItems";
 
 // =========== States & Props ===========
 // GameBoard will manage the main states of the game such as score, energy, items, cat position, and whether the game is playing or paused.
 // These states will be updated based on user interactions and game events.
+const setScore = () => null;
+
+function GameBoard() {
     const [score, setScore] = useState(0);
     const [energy, setEnergy] = useState(100);
     const [catPosition, setCatPosition] = useState({ x: 0, y: 0 });
     const [isPlaying, setIsPlaying] = useState(false);
     const [items, setItems] = useState([]);
-
-
-function GameBoard() {
 
     return (
         <div className="game-board">
@@ -22,7 +22,7 @@ function GameBoard() {
             <HeaderBar score={score} energy={energy} />
 
             <div className = "playing-area">
-                <FallingItems />
+                {/* <FallingItems /> */}
 
                 <div className="cat" style={{ left: catPosition.x, top: catPosition.y }}>
                     <CatPlayer />
