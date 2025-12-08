e7c98222e4312e88bb7abec46acf9af75e8ff5fb
Shu <shuw425@gmail.com>
2025-12-08 02:22:46 -0500

    add falling items config

--- projects/focus_purr-grammer/src/components/FallingItems.jsx
+++ projects/focus_purr-grammer/src/components/FallingItems.jsx
@@ -1,5 +1,16 @@
-import React from "react"
+import React from "react";
+import ITEMS_CONFIG from '../config/ItemConfig';
 
-function FallingItems() {
-  
+function FallingItems({item_type, itemX, itemY}) {
+  const config = ITEMS_CONFIG[time_type];
+
+  if (!config) {
+    console.log(`Unknown item type ${item_type}`);
+    return null;
+
+    return <div>{config.emoji}</div>;
+  }
+
 }
+
+export default FallingItems;
---
+++ projects/focus_purr-grammer/src/config/ItemConfig.jsx
@@ -0,0 +1,46 @@
+import React  from "react";
+
+export const ITEM_CONFIG = {
+  bugs: {
+    category: "work",
+    emoji: "🐛",
+    score: 1,
+    energy: 0
+  },
+  ladyBug: {
+    category: "work",
+    emoji: "🐞",
+    score: 1,
+    energy: 0
+  },
+  worm: {
+    category: "work",
+    emoji: "🪱",
+    score: 1,
+    energy: 0
+  },
+  error404: {
+    category: "work",
+    emoji: "🚫",
+    score: 2,
+    energy: 0
+  },
+  catnip: {
+    category: "distraction",
+    emoji: "🌿",
+    score: -1,
+    energy: 0
+  },
+  yarn: {
+    category: "distraction",
+    emoji: "🧶",
+    score: -1,
+    energy: 0
+  },
+  mouse:{
+    category: "distraction",
+    emoji: "🐭",
+    score: -3,
+    energy: 0
+  }
+}
