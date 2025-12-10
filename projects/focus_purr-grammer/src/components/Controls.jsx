8f7ac7533def338e60b90770e28278faa1e39063
Shu <shuw425@gmail.com>
2025-12-10 00:28:12 -0500

    feat: set cat initial position, add initial button function Next: - falling items logic

--- projects/focus_purr-grammer/src/components/Controls.jsx
+++ projects/focus_purr-grammer/src/components/Controls.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Controls.css';
-function Controls () {
+function Controls ({gameStatus, onStart, onPause, onReset}) {
+
   return (
     <div className='control-area'>
-      <button id="startBtn">Start</button>
-      <button id="pauseBtn">Pause</button>
-      <button id="resetBtn">Reset</button>
+      <button id="startBtn" onClick= {onStart} 
+      disabled ={gameStatus === "running"}>Start</button>
+
+      <button id="pauseBtn" onClick= {onPause}
+      disabled ={gameStatus === "running"}>Pause</button>
+
+      <button id="resetBtn" onClick= {onReset}
+      disabled={gameStatus === "idle"}>Reset</button>
     </div>
   )
 }
--- projects/focus_purr-grammer/src/components/GameBoard.jsx
+++ projects/focus_purr-grammer/src/components/GameBoard.jsx
@@ -9,14 +9,56 @@ import './GameBoard.css';
 // =========== States & Props ===========
 // GameBoard will manage the main states of the game such as score, energy, items, cat position, and whether the game is playing or paused.
 // These states will be updated based on user interactions and game events.
-
+const BOARD_WIDTH = 500;
+const CAT_WIDTH = 60;
 function GameBoard() {
+    // =========== Core States ===========
     const [score, setScore] = useState(0);
     const [energy, setEnergy] = useState(100);
-    const [catPosition, setCatPosition] = useState({ x: 0, y: 0 });
-    const [isPlaying, setIsPlaying] = useState(false);
+
+    // Game Status can be idle, running, paused, over
+    const [gameStatus, setGameStatus] = useState('idle');
+
+    // =========== Cat Status ===========
+    const [catPosition, setCatPosition] = useState(BOARD_WIDTH / 2 - CAT_WIDTH / 2);
+
+    // =========== Falling Items ===========
     const [items, setItems] = useState([]);
 
+
+
+
+    // ========== Button Fucntions =========
+    const handleStart = () => {
+        if(gameStatus === "idle" || gameStatus === "over") {
+            setScore(0);
+            setEnergy(100);
+            setItems([]);
+        }
+        setGameStatus("running")
+
+        // TODOS:  Game running Logic
+    }
+
+    const handlePause = () => {
+        if (gameStatus !== "running") return;
+        setGameStatus("paused");
+        
+        // TODOS:  Game Pause Logic
+    }
+    
+    const handleReset = () => {
+        setGameStatus("idles");
+        setScore(0);
+        setEnergy(100);
+        setCatPosition(BOARD_WIDTH / 2 - CAT_WIDTH / 2);
+
+        // TODOS:  Game Reset Logic
+    }
+
+
+
+    // ========== Returning Fucntions =========
     return (
         <div className="game_board">
             <h1>Focus! Purr-grammer</h1>
@@ -25,13 +67,19 @@ function GameBoard() {
             <div className = "playing_area">
                 {/* <FallingItems /> */}
 
-                <div className="cat" style={{ left: catPosition.x, top: catPosition.y }}>
+                <div className="cat_container" style={{left: catPosition}}>
                     <CatPlayer />
                 </div>
             </div>
 
-            <Controls />
+            <Controls
+                gameStatus={gameStatus}
+                onStart={handleStart}
+                onPause={handlePause}
+                onReset={handleReset}
+            />
         </div>
+      
     )
 }
 
