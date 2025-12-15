2a1228e39b161ee181ce30436eb2c840a33189cd
Shu <shuw425@gmail.com>
2025-12-15 01:01:07 -0500

    change Energy display to focus %

--- projects/focus_purr-grammer/src/components/HeaderBar.jsx
+++ projects/focus_purr-grammer/src/components/HeaderBar.jsx
@@ -4,7 +4,7 @@ function HeaderBar({ score, energy, isMuted, toggleMute }) {
     return (
         <div className = 'header-bar'>
             <div className="score">Score: {score}</div>
-            <div className="energy">Energy: {energy}</div>
+            <div className="energy">Focus: {energy}%</div>
             <div className="sound_btn_container">
                 <button 
                 className="sound_btn"
