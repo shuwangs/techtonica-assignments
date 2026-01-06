dd4f1e07b27108d73db3364997d876411032b20b
Shu <shuw425@gmail.com>
2026-01-06 02:45:52 -0500

    feat: add GET endpoint for blogs

--- projects/blog_api/index.js
+++ projects/blog_api/index.js
@@ -4,28 +4,36 @@ import dotenv from 'dotenv'
 import BLOGS from './blog.js';
 
 dotenv.config({ path: './.env' })
-
-const app = express();
 const port = process.env.PORT || 5000
 
-// [READ] GET
+console.log(BLOGS);
+
+const app = express();
+
+// middlewares
+app.use(cors())
+app.use(express.json());
+
+let blogsData = [...BLOGS];
+// [READ] GET: fetch all the blogs
 app.get('/api/blogs', (req, res) => {
-    res.send("Here will be the display the blogs");
+    res.json(blogsData);
+    // res.send("Here will be the display the blogs");
 })
 
 // [CREATE] POST: add new blog
-app.post('/app/blogs', (req, res) =>{
+app.post('/api/blogs', (req, res) =>{
      res.send("Here will be the create new blogs");
 })
 
 // [UPDATE] PUT: update blogs
-app.put('/app/blogs/:id', (req, res) =>{
+app.put('/api/blogs/:id', (req, res) =>{
      res.send("Here will be the update blogs");
 })
 
 
 // [DELETE] DELETE: update blogs
-app.delete('/app/blogs/:id', (req, res) =>{
+app.delete('/api/blogs/:id', (req, res) =>{
      res.send("Here will be the delete blogs");
 })
 
