1284bf974bb7687967f4ee87dc14b007187dfe18
Shu <shuw425@gmail.com>
2026-01-06 02:22:47 -0500

    feat: define CRUD route placeholders

--- projects/blog_api/index.js
+++ projects/blog_api/index.js
@@ -8,9 +8,25 @@ dotenv.config({ path: './.env' })
 const app = express();
 const port = process.env.PORT || 5000
 
-// Get 
-app.get('/', (req, res) => {
-    res.send("Hello world");
+// [READ] GET
+app.get('/api/blogs', (req, res) => {
+    res.send("Here will be the display the blogs");
+})
+
+// [CREATE] POST: add new blog
+app.post('/app/blogs', (req, res) =>{
+     res.send("Here will be the create new blogs");
+})
+
+// [UPDATE] PUT: update blogs
+app.put('/app/blogs/:id', (req, res) =>{
+     res.send("Here will be the update blogs");
+})
+
+
+// [DELETE] DELETE: update blogs
+app.delete('/app/blogs/:id', (req, res) =>{
+     res.send("Here will be the delete blogs");
 })
 
 
