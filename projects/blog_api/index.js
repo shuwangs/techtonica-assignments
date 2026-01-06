203d2da8537b91358bca8c75c4acedb956eef9be
Shu <shuw425@gmail.com>
2026-01-06 02:13:18 -0500

    feat: add .env to manage the PORT for further deploy to cloud

--- projects/blog_api/index.js
+++ projects/blog_api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv'
 import BLOGS from './blog.js';
 
-const app = express();
-const PORT = 5001;
+dotenv.config({ path: './.env' })
 
+const app = express();
+const port = process.env.PORT || 5000
 
 // Get 
 app.get('/', (req, res) => {
@@ -12,6 +14,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(PORT, ()=> {
-    console.log(`API server listening on port ${PORT}`);
+app.listen(port, ()=> {
+    console.log(`API server listening on port ${port}`);
 })
