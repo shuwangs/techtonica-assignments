f765bb23d9006b13b83ef977472b21224d61ae73
Shu <shuw425@gmail.com>
2026-01-14 03:26:42 -0500

    refactor: extract SQL queries into separate module

--- projects/blog_api/index.js
+++ projects/blog_api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
-// import {writeJson, readJson, validateId} from './utils/utils.js'
+import {writeJson, readJson, validateId} from './utils/utils.js'
+import * as sql_queries from './utils/queries.js';
 import pool from './utils/db.js';
 
 dotenv.config({ path: './.env' })
@@ -16,18 +17,7 @@ app.use(express.json());
 // [READ] GET: fetch all the blogs
 app.get('/api/blogs', async (req, res) => {
      try {
-          const result = await pool.query(
-               `SELECT p.title, p.summary, p.created_at, 
-               cats.name AS category_name, 
-               ARRAY_AGG (tags.name) AS tags
-               FROM posts p
-               LEFT JOIN categories cats on p.category_id = cats.id
-               LEFT JOIN tags_posts tp on p.id = tp.post_id 
-               LEFT JOIN tags on tp.tag_id = tags.id
-               GROUP BY p.title, p.summary, p.created_at, category_name
-               ORDER BY p.created_at DESC`
-          );
-
+          const result = await pool.query(sql_queries.GET_ALL_BLOGS);
           res.json(result.rows);
      } catch (err) {
           res.status(500).json({
@@ -42,19 +32,7 @@ app.get('/api/blogs/:id', async (req, res) => {
      if(!validateId(reqId, res)) return;
      try {
           // const idx = blogsData.findIndex(blog => blog.id === reqId);
-          const result = await pool.query(
-               `SELECT p.title, p.summary, p.created_at, 
-               cats.name AS category_name, 
-               ARRAY_AGG (tags.name) AS tags
-               FROM posts p
-               LEFT JOIN categories cats on p.category_id = cats.id
-               LEFT JOIN tags_posts tp on p.id = tp.post_id 
-               LEFT JOIN tags on tp.tag_id = tags.id
-               WHERE p.id = $1
-               GROUP BY p.title, p.summary, p.created_at, category_name`,
-               [reqId]
-               
-          )
+          const result = await pool.query(sql_queries.GET_SINGLE_BLOG, [reqId] )
           if (result.rows.length === 0) {
                res.status(404).json({
                     error: "BlogNotFound",
@@ -83,18 +61,12 @@ app.post('/api/blogs', async (req, res) => {
 
           // add to categories table
           const catRes = await client.query(
-               `INSERT INTO categories (name)
-               VALUES ($1) ON CONFLICT (name) 
-               DO UPDATE SET name = EXCLUDED.name 
-               RETURNING id`,
-               [category || 'uncategoried']
+               sql_queries.INSERT_INTO_CATEGORIES, [category || 'uncategoried']
           )
           const categoryId = catRes.rows[0].id;
 
           // add to posts table
-          const postRes = await client.query(
-               `INSERT INTO posts (title, summary, content, cover_image_url, created_at,  category_Id)
-               VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
+          const postRes = await client.query(sql_queries.POST_BLOG,
                [title, summary, content, cover_image_url, create_at, categoryId]
           )
           const postId = postRes.rows[0].id;
@@ -102,23 +74,11 @@ app.post('/api/blogs', async (req, res) => {
           // add to tags table
 
           for (const tagName of tags) {
-            const tagRes = await client.query(
-                `INSERT INTO tags(name)
-                VALUES ($1)
-                ON CONFLICT (name)
-                DO UPDATE SET name = EXCLUDED.name
-                RETURNING id`,
-                [tagName]
+            const tagRes = await client.query(sql_queries.INSERT_INTO_TAGS, [tagName]
             )
             const tagId = tagRes.rows[0].id;
 
-            await client.query(
-                `INSERT INTO tags_posts(tag_id, post_id)
-                VALUES ($1, $2)
-                ON CONFLICT DO NOTHING`
-                ,
-                [tagId, postId]
-            )
+            await client.query(sql_queries.INSERT_INTO_TAGS_POSTS,[tagId, postId])
         }
           await client.query('COMMIT');
 
@@ -151,35 +111,23 @@ app.put('/api/blogs/:id', async (req, res) => {
      let categoryId = null;
      try {
           await client.query('BEGIN');
+          console.log("sql_queries keys:", Object.keys(sql_queries));
+          console.log("INSERT_INTO_CATEGORIES:", sql_queries.INSERT_INTO_CATEGORIES);
+          console.log("UPDATE_POST:", sql_queries.UPDATE_POST);
+          console.log("DELETE_TAGS:", sql_queries.DELETE_TAGS);
+          console.log("INSERT_INTO_TAG:", sql_queries.INSERT_INTO_TAGS);
+          console.log("INSERT_INTO_TAGS_POSTS:", sql_queries.INSERT_INTO_TAGS_POSTS);
 
           // 1. update  categories table
           if (category) {
-               const catRes = await client.query(
-                    `INSERT INTO categories (name)
-                    VALUES ($1)
-                    ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name 
-                    RETURNING id`,
-                [category]
-               );
+               const catRes = await client.query(sql_queries.INSERT_INTO_CATEGORIES, [category]);
 
                categoryId = catRes.rows[0].id;
           }
 
-          // 2. update posts table
-          const updatePostQuery = 
-               `UPDATE posts 
-                SET title = COALESCE($1, title), 
-                summary = COALESCE($2, summary), 
-                content = COALESCE($3, content), 
-                cover_image_url = COALESCE($4, cover_image_url),
-                category_id = COALESCE($5, category_id),
-                updated_at = CURRENT_TIMESTAMP
-                WHERE id = $6
-                RETURNING *`;
-          
-               const postRes = await client.query(updatePostQuery, [
-                    title, summary, content, cover_image_url, categoryId, reqIdx
-               ]);
+          // 2. update posts table          
+          const postRes = await client.query(sql_queries.UPDATE_POST, [
+               title, summary, content, cover_image_url, categoryId, reqIdx]);
 
           if (postRes.rowCount === 0) {
                await client.query('ROLLBACK');
@@ -190,30 +138,14 @@ app.put('/api/blogs/:id', async (req, res) => {
           if (tags && Array.isArray(tags)) {
 
                // delete previous tags 
-               await client.query(
-                    `DELETE 
-                     FROM tags_posts 
-                     WHERE post_id = $1`, 
-                    [reqIdx]
-               )
+               await client.query(sql_queries.DELETE_TAGS, [reqIdx])
                
                // add new tags.
                for (const tagName of tags) {
-                const tagRes = await client.query(
-                    `INSERT INTO tags (name) 
-                     VALUES ($1) 
-                     ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name 
-                     RETURNING id`,
-                    [tagName]
-                );
-                const tagId = tagRes.rows[0].id;
+                   const tagRes = await client.query(sql_queries.INSERT_INTO_TAGS, [tagName]);
+                   const tagId = tagRes.rows[0].id;
 
-                await client.query(
-                    `INSERT INTO tags_posts (tag_id, post_id) 
-                     VALUES ($1, $2) 
-                     ON CONFLICT DO NOTHING`,
-                    [tagId, reqIdx]
-                );
+                   await client.query(sql_queries.INSERT_INTO_TAGS_POSTS, [tagId, reqIdx]);
                }
           }
           await client.query('COMMIT');
@@ -236,13 +168,7 @@ app.delete('/api/blogs/:id', async (req, res) =>{
      if(!validateId(reqIdx, res)) return;
 
      try{
-          const result = await pool.query(
-               `DELETE 
-               FROM posts
-               WHERE id = $1 
-               RETURNING *`,
-               [reqIdx]
-          );
+          const result = await pool.query(sql_queries.DELETE_POSTS,[reqIdx]);
           
           if (result.rowCount !== 0) {
                res.json({
