0a8e829fc01c3eb7449cd56e859ba1e7a8b7fcae
Shu <shuw425@gmail.com>
2026-01-21 00:42:14 -0500

    feat: format data that send to front

--- projects/weather-app/server/index.js
+++ projects/weather-app/server/index.js
@@ -24,8 +24,8 @@ app.get('/', (req, res) => {
 app.get('/weather', async (req, res) => {
     const city = req.query.cityName; 
     const params = new URLSearchParams({
-        q: req.query.cityName,
-        appid: process.env.API_KEY,
+        q: city,
+        appid: API_KEY,
         units: "imperial",
     })
     const url = `https://api.openweathermap.org/data/2.5/weather?${params}`
@@ -36,6 +36,24 @@ app.get('/weather', async (req, res) => {
             throw new Error ("Fetch API error");
         }
         const data = await response.json();
+
+
+        const formatedData = {
+            "city": data.name,
+            "country": data.sys.country,
+            "generatedAt": data.dt,
+            "current": {
+            "condition": data.weather[0].main,
+            "description": data.weather[0].description,
+            "icon": data.weather[0].icon,
+            "temp": data.main.temp,
+            "feelsLike": data.main.feels_like,
+            "humidity": data.main.humidity,
+            "windSpeed": data.wind.speed,
+            "cloudCoverage": data.clouds.all
+            }
+        }
+        console.log(formatedData);
         res.json(data)
 
     } catch (error){
