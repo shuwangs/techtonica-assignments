863b705e0b7a5e7567bb4281528ca3b3ed00a6c0
Shu <shuw425@gmail.com>
2026-01-20 23:56:56 -0500

    feat(backend): set up backend server - install packages express, cors, nodemon - add /weather endpoint with OpenWeather interation - basic request validation and errors

---
+++ projects/weather-app/server/index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log("Node runtime:", process.version);
+
+const PORT = process.env.PORT || 5000;
+const API_KEY = process.env.API_KEY;
+
+const app = express();
+
+
+// midware
+app.use(cors());
+app.use(express.json())
+
+// creates an endpoint for the route /api
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from My template ExpressJS' });
+  });
+
+// creates an endpoint for the route /api/weather
+app.get('/weather', async (req, res) => {
+    const city = req.query.cityName; 
+    const params = new URLSearchParams({
+        q: req.query.cityName,
+        appid: process.env.API_KEY,
+        units: "imperial",
+    })
+    const url = `https://api.openweathermap.org/data/2.5/weather?${params}`
+    console.log(url)
+    try{
+        const response = await fetch(url);
+        if(!response.ok) {
+            throw new Error ("Fetch API error");
+        }
+        const data = await response.json();
+        res.json(data)
+
+    } catch (error){
+        console.error("Fetch Failed", error)
+        res.status(500).json({ error: error.message});    }
+});
+
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
