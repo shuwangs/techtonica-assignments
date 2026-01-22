4e8a59e8a260fdf46e33ef0156c364d129c1af0c
Shu <shuw425@gmail.com>
2026-01-22 02:24:18 -0500

    feat(backend): add Redis caching for weather data retrieval - use Redis to cache openweather API response for 1 hr

--- projects/weather-app/server/index.js
+++ projects/weather-app/server/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import redis from 'redis';
 import dotenv from 'dotenv';
 
 dotenv.config();
 console.log("Node runtime:", process.version);
 
-const PORT = process.env.PORT || 5000;
 const API_KEY = process.env.API_KEY;
 
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client =redis.createClient(REDIS_PORT);
+
+client.connect().then(() => {
+    console.log("Connected to Redis! 🚀");
+}).catch(err => {
+    console.error("Redis Connection Error", err);
+});
 const app = express();
 
 
@@ -28,9 +38,17 @@ app.get('/api/weather', async (req, res) => {
         appid: API_KEY,
         units: "imperial",
     })
+
     const url = `https://api.openweathermap.org/data/2.5/weather?${params}`
-    console.log(url)
+    const cacheKey = `weather:${city.toLowerCase().trim()}`;
     try{
+        const cachedData = await client.get(cacheKey);
+        if(cachedData) {
+            console.log("Cache Hit!!!");
+            console.log(cachedData);
+            return res.json(JSON.parse(cachedData));
+        }
+        console.log('Cache Miss, Fetcthing from API');
         const response = await fetch(url);
         if(!response.ok) {
             throw new Error ("Fetch API error");
@@ -53,7 +71,9 @@ app.get('/api/weather', async (req, res) => {
             "cloudCoverage": data.clouds.all
             }
         }
-        console.log(formatedData);
+
+        await client.setEx(cacheKey,3600, JSON.stringify(formatedData));
+
         res.json(formatedData)
 
     } catch (error){
