66358f70e3decc915a2c2d8992563ab6f25cfe13
shu.wang <60482034+shuwangs@users.noreply.github.com>
2026-02-23 20:01:00 +0000

    add testing

--- quizzes/Quiz-RTL-Testing-Input/src/components/ChangeInput/__tests__/ChangeInput.test.js
+++ quizzes/Quiz-RTL-Testing-Input/src/components/ChangeInput/__tests__/ChangeInput.test.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ChangeInput from '../ChangeInput';
 import { render, fireEvent, cleanup } from '@testing-library/react';
+describe('ChangeInput Component', () => {
+    afterEach(cleanup);
+
+    test("display correct greetings message", () => {
+        const { getByLabelText, getByTestId } = render(<ChangeInput />);
+
+        const input = getByLabelText('user-name');
+        const greeting = getByTestId('change-input-greeting');
+        
+        // Initially, the greeting should be empty
+        expect(input.value).toBe('');
+        expect(greeting.textContent).toBe('Welcome, Anonymous User!');
+
+        // Simulate user typing a name
+        fireEvent.change(input, { target: { value: 'Bobo' } });
+        expect(greeting.textContent).toBe('Welcome, Bobo!');
+    });
+})
--- quizzes/Quiz-RTL-Testing-Input/src/components/FocusInput/__tests__/FocusInput.test.js
+++ quizzes/Quiz-RTL-Testing-Input/src/components/FocusInput/__tests__/FocusInput.test.js
@@ -1,3 +1,29 @@
 import React from 'react';
 import FocusInput from '../FocusInput';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render, fireEvent, cleanup, getByLabelText } from '@testing-library/react';
+
+describe('FocusInput Component', () => {
+   afterEach(cleanup);
+   
+    test('handle matching snapshot content', () => {
+        const { container } = render(<FocusInput />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    test('focuses the input when the button is clicked', () => {
+        const { getByText, getByLabelText } = render(<FocusInput />);
+        const button = getByText('Click to Focus');
+        const input = getByLabelText('focus-input');
+
+        // Ensure the input is not focused initially
+        expect(document.activeElement).not.toBe(input);
+
+        // Click the button to focus the input
+        fireEvent.click(button);
+
+        // Assert that the input is now focused
+        expect(document.activeElement).toBe(input);
+    });
+
+
+})
